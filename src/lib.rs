cc2d1e3b96caa25911819d0f75c28f2c5dce79ea
Pièrre <77776198+PierreV23@users.noreply.github.com>
2022-04-18 01:09:04 +0200

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 use core::panic;
+use rand::prelude::SliceRandom;
 use rand::rngs::ThreadRng;
+use rand::seq::index::sample;
 use rand::Rng;
 use std::f64::consts::E;
 use std::iter;
+use std::ops::Range;
 
-type ActivationFunction = fn(f64) -> f64;
+pub type ActivationFunction = fn(f64) -> f64;
 
 // Is of type ActivationFunction
 pub fn sigmoid(n: f64) -> f64 {
@@ -18,7 +21,7 @@ pub struct Neuron {
 }
 
 impl Neuron {
-    pub fn new(number_of_inputs: u32, activation_function: ActivationFunction) -> Self {
+    pub fn new(number_of_inputs: usize, activation_function: ActivationFunction) -> Self {
         if number_of_inputs < 1 {
             panic!("A neuron is required to have atleast one input.")
         }
@@ -28,16 +31,37 @@ impl Neuron {
                 .map(|_| rng.gen_range(-1.0f64..1.0))
                 .collect::<Vec<_>>(),
             bias: 1.,
-            activation_function: activation_function,
+            activation_function,
         }
     }
     pub fn process(&self, input: &Vec<f64>) -> f64 {
         if self.weights.len() != input.len() {
             panic!("The amount of input values has to be the same as the amount of weights this neuron has.")
         }
-        iter::zip(self.weights.iter(), input.iter())
-            .map(|(&a, &b)| a * b)
-            .sum()
+        (self.activation_function)(
+            iter::zip(self.weights.iter(), input.iter())
+                .map(|(&a, &b)| a * b)
+                .chain(std::iter::once(self.bias))
+                .sum(),
+        )
+    }
+    pub fn randomize_weights(&mut self, amount_of_weights: usize, range: Range<f64>) {
+        if amount_of_weights < 1 || amount_of_weights > self.weights.len() {
+            panic!(
+                "Argument `amount_of_weights` has to be 0<n<{}",
+                self.weights.len()
+            )
+        }
+        let mut rng = rand::thread_rng();
+        let chosen = sample(&mut rng, self.weights.len(), amount_of_weights);
+        for idx in chosen.iter() {
+            let weight = self.weights.get_mut(idx).expect("How did you even get this to happen, smh. Anyway rng somehow generated an out of range index. This will probably never happen, why are you reading the code, going to commit plagerism????");
+            *weight += rng.gen_range(range.clone());
+        }
+    }
+    pub fn randomize_bias(&mut self, range: Range<f64>) {
+        let mut rng = rand::thread_rng();
+        self.bias += rng.gen_range(range);
     }
 }
 
@@ -47,8 +71,8 @@ pub struct NeuralLayer {
 
 impl NeuralLayer {
     pub fn new(
-        number_of_neurons: u32,
-        number_of_inputs: u32,
+        number_of_neurons: usize,
+        number_of_inputs: usize,
         activation_function: ActivationFunction,
     ) -> Self {
         Self {
@@ -62,22 +86,25 @@ impl NeuralLayer {
             .iter()
             // TODO?:
             // undo the need to reference input,
-            // this would mean input would have to get multiple times,
-            // so probraly bad idea.
+            // this would mean input would have to get copied multiple times,
+            // so problaby bad idea.
             .map(|neuron| neuron.process(&input))
             .collect::<Vec<f64>>()
     }
+    pub fn get_mut_neurons(&mut self) -> &mut Vec<Neuron> {
+        &mut self.neurons
+    }
 }
 
-struct NeuralNetwork {
-    total_inputs: u32,
+pub struct NeuralNetwork {
+    total_inputs: usize,
     layers: Vec<NeuralLayer>,
 }
 
 impl NeuralNetwork {
     pub fn new(
-        layer_blueprint: Vec<u32>,
-        total_inputs: u32,
+        layer_blueprint: Vec<usize>,
+        total_inputs: usize,
         activation_function: Option<ActivationFunction>,
     ) -> Self {
         if layer_blueprint.len() < 1 {
@@ -108,6 +135,27 @@ impl NeuralNetwork {
             total_inputs,
             layers,
         }
+    }
+    pub fn process(&self, inputs: Vec<f64>) -> Vec<f64> {
+        if self.total_inputs != inputs.len() {
+            panic!("The amount of inputs is incorrect, this network was made to take {} inputs, yet you gave {}", self.total_inputs, inputs.len())
+        }
+        Vec::new()
+    }
+    pub fn get_mut_all_neurons(&mut self) -> Vec<&mut Neuron> {
+        self.layers
+            .iter_mut()
+            .flat_map(|layer| layer.get_mut_neurons())
+            .collect::<Vec<&mut Neuron>>()
+    }
+    pub fn evolve(
+        &mut self,
+        input: Vec<f64>,
+        output: Vec<f64>,
+        children_per_generation: usize,
+        generations: usize,
+    ) {
+        ()
     }
 }
 
